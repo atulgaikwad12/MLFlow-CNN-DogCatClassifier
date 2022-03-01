2028368ace32a16fd9ff7422f02df0f410cc55ad
atulgaikwad12 <atulanilgaikwad12@gmail.com>
2022-03-02 01:26:26 +0530

    layout of callback stage added for future work

---
+++ src/stage_03_prepare_callback.py
@@ -0,0 +1,42 @@
+import argparse
+import os
+import logging
+import tensorflow as tf
+from src.utils.common import create_directories, read_yaml
+
+
+STAGE = "Callback Addition" ## <<< change stage name 
+LOG_DIR = "logs"
+LOG_FILENAME = "running_logs.log"
+
+logging.basicConfig(
+            filename = os.path.join(LOG_DIR, LOG_FILENAME), 
+            level=logging.INFO, 
+            format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+            filemode="a"
+            )
+
+
+def main(config_path):
+    ## read config files
+    config = read_yaml(config_path)
+    
+    params = config["params"]
+    # Need to add Callback part here 
+
+    
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="configs/config.yml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
