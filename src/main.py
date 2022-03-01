341724d232f0151c5b448b5751db36a5c511a38e
atulgaikwad12 <atulanilgaikwad12@gmail.com>
2022-03-02 01:38:24 +0530

    uncommented mlflow pipeline

--- src/main.py
+++ src/main.py
@@ -2,25 +2,29 @@ import mlflow
 # import argparse
 import os
 import logging
+from src.utils.common import create_directories
 
 STAGE = "MAIN"  #Stage name for logs 
+LOG_DIR = "logs"
+LOG_FILENAME = "running_logs.log"
+
+create_directories(LOG_DIR)
 
 logging.basicConfig(
-    filename=os.path.join("logs","running_logs.log"),
+    filename=os.path.join(LOG_DIR,LOG_FILENAME),
     level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s] : %(message)s",
     filemode="a"
 )
 
 def main():
-    pass
-    '''
+ 
     with mlflow.start_run() as run:
-        #mlflow.run(".","get_data",parameters={},useConda="false")
-        mlflow.run(".","get_data",useConda="false")
-        mlflow.run(".","base_model_creation",useConda="false")
-        mlflow.run(".","training",useConda="false")
-    '''
+        # mlflow.run(".","get_data",parameters={},use_conda="false") # alternate way to pass parameters
+        mlflow.run(".","get_data",use_conda="false")
+        mlflow.run(".","base_model_creation",use_conda="false")
+        mlflow.run(".","training",use_conda="false")
+    
 
 if(__name__ == "__main__"):
     try:
