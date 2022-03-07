2a58b9bb0c1ff90582ac7de66735c13d03c3775b
atulgaikwad12 <atulanilgaikwad12@gmail.com>
2022-03-07 19:51:10 +0530

    different callbacks added in stage 3

--- src/stage_03_prepare_callback.py
+++ src/stage_03_prepare_callback.py
@@ -3,6 +3,7 @@ import os
 import logging
 import tensorflow as tf
 from src.utils.common import create_directories, read_yaml,get_unique_log_path
+from src.utils.custom_callbacks import ThresholdAccuracy
 import pickle
 
 STAGE = "Callback Addition" ## <<< change stage name 
@@ -16,57 +17,92 @@ logging.basicConfig(
             filemode="a"
             )
 
-class customCallback(tf.keras.callbacks.Callback):
-    def __init__(self,ACCURACY_THRESHOLD) -> None:
-        super().__init__()
-        self.ACCURACY_THRESHOLD = ACCURACY_THRESHOLD
-        logging.info("Preparing custom callbacks...")
-    
-    def on_epoch_end(self, epoch, logs={}):
-        if(logs.get('acc') > self.ACCURACY_THRESHOLD):
-            logging.info("\nReached %2.2f%% accuracy, so stopping training!!" %(self.ACCURACY_THRESHOLD*100))
-            self.model.stop_training = True
 
+def save_callbacks(callbacks_obj_lst, file_path):
+
+    if(len(callbacks_obj_lst) > 0 and os.path.exists(file_path) ):
+        file = open(file_path, 'w') 
+        pickle.dump(callbacks_obj_lst, file)
+        logging.info(f"Saved callbacks object list in file {file_path}")
 
 def main(config_path):
-    callbacks_lst = []
+    callbacks_obj_lst = [] # list to save callback object
 
     ## read config files
     config = read_yaml(config_path)
-    #config["callbacks"]
-    artifacts = config["artifacts"]
+    cb_params = config["callbacks"] # callback related parameters 
+    artifacts = config["artifacts"] # directory path info
 
-    callbacks_dir = artifacts["CALLBACKS_DIR"] 
-    create_directories([callbacks_dir])
+    cblist = cb_params["cblist"] # list of callbacks defined 
+    if(len(cblist) > 0):
+        # when we have callbacks to prepare
 
-    callbacksbyte_file = os.path.join(
-    artifacts["CALLBACKS_DIR"],
-    artifacts["CB_BYTECODE_FNAME"])
+        callbacks_dir = artifacts["CALLBACKS_DIR"] 
+        create_directories([callbacks_dir])
 
-    # Need to add Callback part here 
+        for callback in cblist:
+            if (callback == "ModelCheckpoint"):
 
-    checkpoint_filepath = '/tmp/checkpoint'
-    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath,
-    save_weights_only=True,
-    monitor='val_accuracy',
-    mode='max',
-    save_best_only=True)
+                checkpoint_dir = os.path.join(callbacks_dir,cb_params[callback]["CHECKPOINT_DIR"]) 
+                create_directories([checkpoint_dir])
 
-    log_dir = get_unique_log_path(artifacts["BASE_LOG_DIR"])
-    #%tensorboard --logdir logs/fit
-    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-    early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=5, restore_best_weights=True)
-    lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(
-    monitor="loss", factor=0.1, patience=3, mode="max", verbose=1
-)
+                filename = cb_params[callback]["filename"] #"weights.{epoch:02d}-{val_loss:.2f}.hdf5"
+                checkpoint_filepath = os.path.join(checkpoint_dir,filename)
 
-    ACCURACY_THRESHOLD = 0.95
-    customCallback(ACCURACY_THRESHOLD)
+                model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+                                    filepath=checkpoint_filepath,
+                                    save_weights_only=True,
+                                    monitor='val_accuracy',
+                                    mode='max',
+                                    save_best_only=True)
 
-    if(os.path.exists(callbacks_dir) and len(callbacks_lst) > 0):
-        file = open(callbacksbyte_file, 'w') 
-        pickle.dump(callbacks_lst, file)
+                callbacks_obj = model_checkpoint_callback
+            elif(callback == "ThresholdAccuracy"):
+
+                ACCURACY_THRESHOLD = cb_params[callback]["ACCURACY_THRESHOLD"]
+                callbacks_obj = ThresholdAccuracy(ACCURACY_THRESHOLD)
+
+            elif(callback == "TensorBoardLog"):
+                log_dir = get_unique_log_path(cb_params[callback]["TENSORBOARD_ROOT_LOG_DIR"])
+                #%tensorboard --logdir logs/fit
+                tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+                callbacks_obj = tensorboard_cb
+
+            elif(callback == "EarlyStopping"):
+
+                patience = cb_params[callback]["patience"]
+                restore_best_weights = cb_params[callback]["restore_best_weights"]
+
+                early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience = patience,
+                                    restore_best_weights = restore_best_weights)
+                callbacks_obj = early_stopping_cb   
+
+            elif(callback == "ReduceLROnPlateau"):
+
+                monitor = cb_params[callback]["monitor"]
+                factor = cb_params[callback]["factor"]
+                patience = cb_params[callback]["patience"]
+                mode = cb_params[callback]["mode"]
+                verbose = cb_params[callback]["verbose"]
+
+                lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor=monitor, 
+                                factor=factor, patience=patience, mode=mode, verbose=verbose)
+                callbacks_obj = lr_scheduler
+
+            else:
+                logging.warning(f"{callable} not defined !!")
+                continue
+
+            callbacks_obj_lst.append(callbacks_obj)
+
+        callbacksbyte_file = os.path.join(callbacks_dir,cb_params["CB_BYTECODE_FNAME"]) 
+        logging.info(f"Saving list of callback objects .....\n{callbacks_obj_lst} ")
+        save_callbacks(callbacks_obj_lst = callbacks_obj_lst,
+                        file_path = callbacksbyte_file)
+        
+    else:
+        logging.warning("No callback found in config file .....")
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
